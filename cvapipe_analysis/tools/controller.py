74dbac6f96072b6d0b2bea112f2ea761addab77b
Amil Khan <amil@ucsb.edu>
2023-07-18 17:01:58 -0700

    Update controller.py

--- cvapipe_analysis/tools/controller.py
+++ cvapipe_analysis/tools/controller.py
@@ -48,7 +48,7 @@ class Controller:
             
         NOTE: Written by Amil for web application version of cvapipe.
         """
-        return Path(self.loaddata_path)
+        return self.loaddata_path
         
 
     def get_staging(self):  # shortcut
